ebbbfca0758b1e1f7af3e0ed144c1f4487af7bd7
Mikhail Garkusha <mgarkusha@mail.ru>
2016-10-26 17:43:48 +0300

    Django Education

---
+++ mysite/aboutme/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.main_page, name='main_page'),
+    url(r'^education', views.education, name='education'),
+    url(r'^work', views.work, name='work'),
+]
---
+++ mysite/aboutme/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def main_page(request):
+    return render(request, 'test.html', {})
+
+def education(request):
+    return render(request, 'education.html', {})
+
+def work(request):
+    return render(request, 'work.html', {})
