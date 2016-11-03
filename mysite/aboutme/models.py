b58d0a9d8f5e9fef0dceeebbc7a122ee3499a8c4
Mikhail Garkusha <mgarkusha@mail.ru>
2016-11-03 19:02:45 +0300

    Updaet

--- mysite/aboutme/models.py
+++ mysite/aboutme/models.py
@@ -5,7 +5,16 @@ from django.db import models
 class Work(models.Model):
     organization = models.CharField(verbose_name='Организация', max_length=32)
     region = models.CharField(verbose_name='Регион', max_length=32, blank=True)
-    site = models.CharField(verbose_name='Сайт', max_length=32, blank=True)
     position = models.CharField(verbose_name='Должность', max_length=32)
-    duties = models.TextField(verbose_name='Обязанности')
-    period = models.PositiveIntegerField(verbose_name='Время работы', default=1)
+
+class Education(models.Model):
+    organization = models.CharField(verbose_name='Наименование учреждения', max_length=32)
+    learn_time = models.PositiveIntegerField(verbose_name='Период учёбы')
+    profession = models.CharField(verbose_name='Учёная степень', max_length=32)
+
+class About(models.Model):
+    fio = models.CharField(verbose_name='ФИО', max_length=32)
+    birthday = models.CharField(verbose_name='Дата рождения', max_length=32)
+    profession = models.CharField(verbose_name='Специальность', max_length=32)
+    born_place = models.CharField(verbose_name='Место рождения', max_length=32)
+    hobby = models.CharField(verbose_name='Хобби', max_length=32)
--- mysite/aboutme/views.py
+++ mysite/aboutme/views.py
@@ -1,34 +1,21 @@
 from django.shortcuts import render_to_response
 from .models import Work
+from .models import Education
+from .models import About
 import datetime
 
 
 # Create your views here.
 def main_page(request):
-    birthday = datetime.date(day=25, month=11, year=1983)
-    aboutme = [{'name': 'ФИО', 'value': 'Гаркуша Михаил Иванович'}, {'name': 'День рождения', 'value': birthday}, {
-        'name': 'Специальность', 'value': 'Инженер связи'},
-               {'name': 'Место рождения', 'value': 'Leningrad'},
-               {'name': 'Интересы', 'value': 'Спорт, путешествия, развитие'}]
+    aboutme = About.objects.all()
     return render_to_response('index.html', {'aboutme': aboutme})
 
 
 def education(request):
-    edu = [{'name': 'Школа №95', 'value': 'с 1 по 9 класс'}, {'name': 'Школа №137', 'value': 'с 9 по 11 класс'},
-           {'name': 'ВУЗ',
-            'value': 'Санкт-Петербургский университет телекоммуникций им. проф. Бонч-Бруевича, Профессия: инженер связи'},
-           {'name': 'Курсы', 'value': 'Много разных, последний Geek Brains'}]
+    edu = Education.objects.all()
     return render_to_response('education.html', {'edu': edu})
 
 
 def work(request):
-    workplaces = [{'name': 'Телефонная компания', 'prof': 'продажа карточек связи'},
-                  {'name': 'Веб-студия', 'prof': 'верстальщик'},
-                  {'name': 'Тур-фирма', 'prof': 'системный администратор'},
-                  {'name': 'Веб-студия', 'prof': 'вёрстка/программирование Perl'},
-                  {'name': 'Завод', 'prof': 'системный админ'},
-                  {'name': 'Курьерская компания', 'prof': 'системный админ'},
-                  {'name': 'Завод', 'prof': 'системный админ'},
-                  {'name': 'Таксомоторная компания', 'prof': 'системный админ'}]
-    workplaces_bd = Work.objects.all()
-    return render_to_response('work.html', {'workplaces': workplaces, 'workplaces_bd': workplaces_bd})
+    workplaces = Work.objects.all()
+    return render_to_response('work.html', {'workplaces': workplaces})
